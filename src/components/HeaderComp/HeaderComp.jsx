bcf5e0a8737ecfea6852fbf1d43518308724fcbc
DanyloOhurtsov <118742794+DanyloOhurtsov@users.noreply.github.com>
2023-11-27 17:00:33 +0100

    Header component
    Changes:
    - begin of creating header component

--- src/components/HeaderComp/HeaderComp.jsx
+++ src/components/HeaderComp/HeaderComp.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import styles from './headerComp.module.scss'
+import { allLogos } from '../../content/logos/allLogos'
 
 
 export const HeaderComp = () => {
   return (
-    <div>HeaderComp</div>
+    <div className={styles.headerComp}>
+      <div className="desktopHeaderContent">
+        <NavLink to={'/'}>
+          <img src="" alt="" />
+        </NavLink>
+      </div>
+    </div>
   )
 }
---
+++ src/content/logos/allLogos.js
@@ -0,0 +1,11 @@
+import darkLogo from "./darkLogo.png";
+import lightLogo from "./lightLogo.png";
+import orangeLogo from "./orangeLogo.png";
+import logoWithoutText from "./logoWithoutText.png";
+
+export const allLogos = {
+    darkLogo,
+    lightLogo,
+    orangeLogo,
+    logoWithoutText,
+};
